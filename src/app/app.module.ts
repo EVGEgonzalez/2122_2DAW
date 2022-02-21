6cf22c8f14c8f9d6a1d2b2d3f2978865fb97acdc
mperezpizarro <marioperezpizarro@gmail.com>
2022-02-21 10:07:19 +0100

    Formulario de alta y credenciales PHP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormularioLoginComponent } from './formulario-login/formulario-login.component';
 import { InputsFormularioComponent } from './inputs-formulario/inputs-formulario.component';
+
+//Conexión http
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,8 @@ import { InputsFormularioComponent } from './inputs-formulario/inputs-formulario
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/config/config.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http/";
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConfigService {
+ 
+
+  constructor(private http: HttpClient) { 
+    
+  }
+
+  // loginCorrecto(){
+  //   return this.http.get("../assets/config-login.json", {responseType: 'json'});
+  // }
+
+
+}
