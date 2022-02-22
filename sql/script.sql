0e418d9942d403cc004eb974ae218bc8657e58bc
usasergio <usasergio2@gmail.com>
2022-02-22 14:07:50 +0100

    Update script.sql

--- sql/script.sql
+++ sql/script.sql
@@ -28,6 +28,17 @@ CONSTRAINT FK_poblaciones FOREIGN KEY (idPoblacion) REFERENCES poblaciones(idPob
 CONSTRAINT FK_etapas FOREIGN KEY (idEtapa) REFERENCES etapas(idEtapa)
 );
 
+CREATE TABLE Cuadernos (
+    idCuaderno int UNSIGNED AUTO_INCREMENT NOT NULL,
+    idUsuario smallint NOT NULL UNIQUE,
+    fechaCreacion datetime NOT NULL DEFAULT NOW(),
+    titulo varchar(100) NOT NULL,
+    dedicatoria varchar(255) NULL,
+
+    CONSTRAINT PK_idCuaderno PRIMARY KEY(idCuaderno),
+    CONSTRAINT FK_idUsuario FOREIGN KEY (idUsuario) REFERENCES usuarios(idUsuario) ON DELETE CASCADE
+);
+
 CREATE TABLE Vivencias (
     idVivencias int UNSIGNED AUTO_INCREMENT NOT NULL,
     fechaCreacion datetime NOT NULL DEFAULT NOW(),
