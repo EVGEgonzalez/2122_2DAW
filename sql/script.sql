0c140f2d53ed104a881a1ec9b72e4e864c9fb4d2
usasergio <usasergio2@gmail.com>
2022-02-22 19:04:55 +0100

    Movido el script corregido de sql

--- sql/script.sql
+++ sql/script.sql
@@ -9,14 +9,14 @@ CREATE TABLE Usuarios (
 
 CREATE TABLE Cuadernos (
     idCuaderno int UNSIGNED AUTO_INCREMENT NOT NULL,
-    idUsuario smallint NOT NULL,
+    idUsuario smallint NOT NULL UNIQUE,
     fechaCreacion datetime NOT NULL DEFAULT NOW(),
-    titulo varchar(100) NOT NULL,
-    dedicatoria varchar(255) NULL,
+    textoPortada varchar(100) NOT NULL,
+    textoContraportada varchar(65535) NULL,
+    imagen varchar(40) NULL,
 
     CONSTRAINT PK_idCuaderno PRIMARY KEY(idCuaderno),
     CONSTRAINT FK_idUsuario FOREIGN KEY (idUsuario) REFERENCES usuarios(idUsuario) ON DELETE CASCADE
 );
 
--- Insertar usuario por defecto...
 INSERT INTO usuarios(nombreUsuario) VALUES("testUser");
