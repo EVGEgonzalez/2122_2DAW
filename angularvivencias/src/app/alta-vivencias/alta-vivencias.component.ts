6cbb849b01d10946a6a372ef43a264cab7a7648c
juanmaMTR <jtoscanoreyes.guadalupe@alumnado.fundacionloyola.net>
2022-02-24 10:24:00 +0100

    Archivos comentados

--- angularvivencias/src/app/alta-vivencias/alta-vivencias.component.ts
+++ angularvivencias/src/app/alta-vivencias/alta-vivencias.component.ts
@@ -12,13 +12,13 @@ import { VivenciasService } from '../vivencias.service';
 })
 
 export class AltaVivenciasComponent implements OnInit {
-
+  /**Objero Vivencias donde se meterán los datos del formulario */
   vivencia: Vivencia = {
     etapa: 0,
     descripcion: "descripcion1",
     imagen: null
   }
-
+  /** Objeto json para pasar al php*/
   json = {
     accion: 'vivencias.alta',
     token: "identificación del usuario",
@@ -28,13 +28,17 @@ export class AltaVivenciasComponent implements OnInit {
       foto: this.vivencia.imagen
     }
   }
-
+  /** @param vivenciasServicio que instacia la clase que está en vivencias.service.ts  */
   constructor(private vivenciasServicio: VivenciasService) { }
 
   etapas: Etapas[] = ETAPAS
 
   ngOnInit(): void {
   }
+  /**
+   * Funcion enviar() para enviar el json al archivo php
+   * @param url Será el archivo php al que voy a llamar
+   */
   enviar(url: string): void {
     console.log('tron1');
     //let json = '{"accion":"vivencias.alta","token":"identificación del usuario","datos":{"idEtapa":' + this.vivencia.etapa + ',"texto":' + this.vivencia.descripcion + ',"foto":' + this.vivencia.imagen + '}}'
--- angularvivencias/src/app/listar-vivencias/listar-vivencias.component.ts
+++ angularvivencias/src/app/listar-vivencias/listar-vivencias.component.ts
@@ -7,13 +7,19 @@ import { VivenciasService } from '../vivencias.service';
   styleUrls: ['./listar-vivencias.component.css']
 })
 export class ListarVivenciasComponent implements OnInit {
+  /**
+   * Creo el objet json que necesito para enviar la accion que tiene que hacer
+   */
   json = {
     accion: 'vivencias.listar',
     token: "identificación del usuario",
   }
   datos=[]
   constructor(private vivenciasServicio: VivenciasService) { }
-
+  /**
+   * ngOnInit() función que se ejecuta al iniciar el componente que tiene el método enviar 
+   * de las vivenciasServicio que llama al archivo php y envia el json creado anteriormente.
+   */
   ngOnInit(): void {
     console.log('Hola mundo');
     
