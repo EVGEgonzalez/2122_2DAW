27a90c358802165898c70408508bc07495156535
2DAW04 <2DAW04@esvirgua.es>
2022-03-03 09:02:09 +0100

    Script individual actualizado

--- sql/script.sql
+++ sql/script.sql
@@ -1,5 +1,8 @@
 CREATE DATABASE cuadernosDB;
 USE cuadernosDB;
+
+
+-- ESTA TABLA SE CREA PARA LA FOREIGN KEY
 CREATE TABLE Usuarios (
     idUsuario smallint NOT NULL AUTO_INCREMENT,
     nombreUsuario varchar(100) NOT NULL,
@@ -7,16 +10,15 @@ CREATE TABLE Usuarios (
 );
 
 
+-- TABLA PRINCIPAL CUADERNOS
 CREATE TABLE Cuadernos (
-    idCuaderno int UNSIGNED AUTO_INCREMENT NOT NULL,
-    idUsuario smallint NOT NULL UNIQUE,
+    idCuaderno smallint UNSIGNED AUTO_INCREMENT NOT NULL,
+    idUsuario smallint UNSIGNED NOT NULL UNIQUE,
     fechaCreacion datetime NOT NULL DEFAULT NOW(),
-    textoPortada varchar(100) NOT NULL,
-    textoContraportada varchar(65535) NULL,
+    textoPortada varchar(1500) NOT NULL,
+    textoContraportada varchar(1500) NULL,
     imagen varchar(40) NULL,
 
     CONSTRAINT PK_idCuaderno PRIMARY KEY(idCuaderno),
     CONSTRAINT FK_idUsuario FOREIGN KEY (idUsuario) REFERENCES usuarios(idUsuario) ON DELETE CASCADE
 );
-
-INSERT INTO usuarios(nombreUsuario) VALUES("testUser");
