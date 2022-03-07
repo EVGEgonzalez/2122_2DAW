42b23f48f90fd849b9e509700000f1160610703e
kiliann <wilian_kilian@hotmail.com>
2022-03-07 12:10:47 +0100

    modificar etapa

---
+++ src/app/modificar-etapa/modificar-etapa.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modificar-etapa',
+  templateUrl: './modificar-etapa.component.html',
+  styleUrls: ['./modificar-etapa.component.css']
+})
+export class ModificarEtapaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
