bb5daf43b05331423efcf33520d0fbfebcc233cc
kiliann <wilian_kilian@hotmail.com>
2022-03-07 10:16:01 +0100

    modificar etapa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AltaEtapaComponent } from './alta-etapa/alta-etapa.component';
 import { BorradoEtapasComponent } from './borrado-etapas/borrado-etapas.component';
 import { ListarEtapasComponent } from './listar-etapas/listar-etapas.component';
+import {ModificarEtapaComponent} from './modificar-etapa/modificar-etapa.component';
 
 const routes: Routes = [
   {path: 'altaetapas', component: AltaEtapaComponent},
   {path: 'borradoetapas', component: BorradoEtapasComponent},
-  {path: 'listadoetapas', component: ListarEtapasComponent}
+  {path: 'listadoetapas', component: ListarEtapasComponent},
+  {path: 'modificaretapas', component: ModificarEtapaComponent}
 ];
 
 @NgModule({
